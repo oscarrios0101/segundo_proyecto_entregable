a2924289616c2d084afe36f2872d82afbc1ce7cd
Oscar Rios <145062464+oscarrios0101@users.noreply.github.co>
2024-11-27 03:15:54 -0600

    Minimum viable product implemented

--- main.js
+++ main.js
@@ -2,13 +2,18 @@ const productWrapper = document.querySelector(".grid-wrapper");
 const productTemplate = document.getElementById("product-template").content;
 const productsArray = [];
 //fetching products form api
-window.addEventListener("DOMContentLoaded", () => {
-  fetch("https://fakestoreapi.com/products/category/jewelery")
-    .then((res) => res.json())
-    .then((products) => {
-      productsArray.push(...products);
-      populateGrid(productsArray);
-    });
+window.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const response = await fetch(
+      "https://fakestoreapi.com/products/category/jewelery"
+    );
+    const products = await response.json();
+
+    productsArray.push(...products);
+    populateGrid(productsArray);
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
 });
 
 function populateGrid(productsArray) {
@@ -18,7 +23,7 @@ function populateGrid(productsArray) {
     card.querySelector(".card__image").alt = product.title;
     card.querySelector(".card__title").textContent = product.title;
     card.querySelector(".card__description").textContent = product.description;
-    card.querySelector(".card__price").textContent = product.price;
+    card.querySelector(".card__price").textContent = `$${product.price}`;
 
     productWrapper.appendChild(card);
   });
