29ed1dfccc028cd85f2976137c095f4e6e12f4bf
Oscar Rios <145062464+oscarrios0101@users.noreply.github.co>
2024-11-28 01:42:03 -0600

    fetching two endpoints with axios as requested

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const productWrapper = document.querySelector(".grid-wrapper");
 const productTemplate = document.getElementById("product-template").content;
 let productsArray = [];
 // Fetching products with Axios
-
+//fetching two endpoints as requested
 const apiUrl = "https://fakestoreapi.com/products/category/jewelery";
 const apiProduct = "https://fakestoreapi.com/products/";
 window.addEventListener("DOMContentLoaded", async () => {
@@ -38,9 +38,37 @@ productWrapper.addEventListener("click", async (event) => {
       const response = await axios.get(`${apiProduct}/${productId}`);
       const productDetails = response.data;
 
-      console.log(productDetails);
+      // Add the product to the cart and update the cart items list
+      addToCart(productDetails);
     } catch (error) {
       console.error(`Error fetching product details: ${error.message}`);
+
+      alert(
+        "An error occurred while fetching product details. Please try again later."
+      );
     }
+  }
+});
+
+function addToCart(productDetails) {
+  const cartItems = document.querySelector(".cart__items");
+  const newItem = document.createElement("li");
+  newItem.classList.add("cart__item");
+  newItem.textContent = `${productDetails.title} - $${productDetails.price}`;
+  cartItems.appendChild(newItem);
+}
+//function to show cart
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("cart-icon")) {
+    const cart = document.querySelector(".cart");
+    cart.classList.toggle("show");
+  }
+});
+
+//function to close cart
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("close-icon")) {
+    const cart = document.querySelector(".cart");
+    cart.classList.remove("show");
   }
 });
